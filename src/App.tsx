598800a950846c0f474d375ee3b223ae18c7089c
Gabo Acosta <gabo.acosta624@gmail.com>
2024-02-27 13:10:50 -0700

    Adds firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getHeaders, singInWithGoogle } from "./util/firebase";
+
+export interface User {
+  id: number
+  email: string
+}
+
 
 function App() {
+  const [users, setUsers] = React.useState<User[]>([])
+  const getUsers = async (): Promise<void> => {
+    const headers = await getHeaders()
+    const url = `/api/users`
+    const res = await fetch(url, {
+      method: "GET",
+      headers: headers,
+      redirect: "follow",
+    })
+    if (res.status !== 200) {
+      console.error(res)
+      throw new Error("Error al cargar Proyectos")
+    }
+    const { results } = await res.json()
+    const newUsers: User[]  = results
+    setUsers(newUsers)
+  }
+  const onLoginClick = () => {
+    singInWithGoogle()
+        .then((result) => {
+          console.trace(result)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+                Gabo's site!
+            </h1>
+            <button onClick={() => onLoginClick()}>Click to Login!</button>
+            <button onClick={() => getUsers()}>Click to get users!</button>
+            <h3>Users:</h3>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.email}>
+                        {user.email}
+                    </li>
+                ))}
+            </ul>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
     </div>
   );
 }
--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -1,21 +1,40 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDcEAh8_1ubBfOOau5y8uouMNiZXudvwMc",
-    authDomain: "django-react-firebase-ae183.firebaseapp.com",
-    projectId: "django-react-firebase-ae183",
-    storageBucket: "django-react-firebase-ae183.appspot.com",
-    messagingSenderId: "681210263108",
-    appId: "1:681210263108:web:b43b72c44b3d7400f5ee11",
-    measurementId: "G-T4SEQX46G1"
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+export const getHeaders = async (): Promise<Headers> => {
+    const currentUser = auth.currentUser;
+    if (!currentUser) {
+        throw new Error("No ha iniciado sesión")
+    }
+    const token = await currentUser.getIdToken()
+    const autorization = `Bearer ${token}`
+    const headers = new Headers({
+        Authorization: autorization,
+        "Content-Type": "application/json",
+    })
+    return headers
+}
+
+export const singInWithGoogle = () => {
+    return signInWithPopup(auth, new GoogleAuthProvider())
+}
